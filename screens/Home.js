0154e196c012a7e5a91d14d9d166336e574591b6
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-04-10 20:43:11 +0200

    Add income dumply data

--- screens/Home.js
+++ screens/Home.js
@@ -19,208 +19,111 @@ import {Svg} from 'react-native-svg';
 
 import { COLORS, FONTS, SIZES, icons, images } from '../constants';
 
-const Home = () => {
+const icons = {
+    education: "education icon",
+    refund: "refund icon",
+    interest: "interest icon",
+    grant: "grant icon",
+    investment: "investment icon",
+  };
+  
+  const income = "income";
+  const expense = "expense";
+  
+  const COLORS = {
+    yellow: "yellow",
+    blue: "blue",
+    green: "green",
+    red: "red",
+    purple: "purple",
+  };
+    
 
-    // dummy data
-    const confirmStatus = "C"
-    const pendingStatus = "P"
+const Home = () => {
 
     let categoriesData = [
         {
-            id: 1,
-            name: "Education",
-            icon: icons.education,
-            color: COLORS.yellow,
-            expenses: [
-                {
-                    id: 1,
-                    title: "Tuition Fee",
-                    description: "Tuition fee",
-                    location: "ByProgrammers' tuition center",
-                    total: 100.00,
-                    status: pendingStatus
-                },
-                {
-                    id: 2,
-                    title: "Arduino",
-                    description: "Hardward",
-                    location: "ByProgrammers' tuition center",
-                    total: 30.00,
-                    status: pendingStatus
-                },
-                {
-                    id: 3,
-                    title: "Javascript Books",
-                    description: "Javascript books",
-                    location: "ByProgrammers' Book Store",
-                    total: 20.00,
-                    status: confirmStatus
-                },
-                {
-                    id: 4,
-                    title: "PHP Books",
-                    description: "PHP books",
-                    location: "ByProgrammers' Book Store",
-                    total: 20.00,
-                    status: confirmStatus
-                }
-            ],
+          id: 1,
+          name: "Ventes",
+          icon: icons.shopping,
+          cat: income,
+          color: COLORS.yellow,
+          data: [
+            {
+              id: 1,
+              description: "Vente 1kg de Legumes frais",
+              total: 100.00,
+              date: "2023-04-10"
+            },
+          ],
         },
         {
-            id: 2,
-            name: "Nutrition",
-            icon: icons.food,
-            color: COLORS.lightBlue,
-            expenses: [
-                {
-                    id: 5,
-                    title: "Vitamins",
-                    description: "Vitamin",
-                    location: "ByProgrammers' Pharmacy",
-                    total: 25.00,
-                    status: pendingStatus,
-                },
-
-                {
-                    id: 6,
-                    title: "Protein powder",
-                    description: "Protein",
-                    location: "ByProgrammers' Pharmacy",
-                    total: 50.00,
-                    status: confirmStatus,
-                },
-
-            ],
+          id: 2,
+          name: "Remboursements",
+          icon: icons.refund,
+          cat: income,
+          color: COLORS.blue,
+          data: [
+            {
+              id: 1,
+              description: "Frais de déplacement pour la réunion avec le client",
+              total: 50.00,
+              date: "2023-04-11"
+            },
+            {
+              id: 2,
+              description: "Déjeuner avec le client",
+              total: 25.00,
+              date: "2023-04-12"
+            },
+          ],
         },
         {
-            id: 3,
-            name: "Child",
-            icon: icons.baby_car,
-            color: COLORS.darkgreen,
-            expenses: [
-                {
-                    id: 7,
-                    title: "Toys",
-                    description: "toys",
-                    location: "ByProgrammers' Toy Store",
-                    total: 25.00,
-                    status: confirmStatus,
-                },
-                {
-                    id: 8,
-                    title: "Baby Car Seat",
-                    description: "Baby Car Seat",
-                    location: "ByProgrammers' Baby Care Store",
-                    total: 100.00,
-                    status: pendingStatus,
-                },
-                {
-                    id: 9,
-                    title: "Pampers",
-                    description: "Pampers",
-                    location: "ByProgrammers' Supermarket",
-                    total: 100.00,
-                    status: pendingStatus,
-                },
-                {
-                    id: 10,
-                    title: "Baby T-Shirt",
-                    description: "T-Shirt",
-                    location: "ByProgrammers' Fashion Store",
-                    total: 20.00,
-                    status: pendingStatus,
-                },
-            ],
+          id: 3,
+          name: "Intérêts",
+          icon: icons.interest,
+          cat: income,
+          color: COLORS.green,
+          data: [
+            {
+              id: 1,
+              description: "Intérêts sur les comptes d'épargne",
+              total: 10.00,
+              date: "2023-04-15"
+            },
+          ],
         },
         {
-            id: 4,
-            name: "Beauty & Care",
-            icon: icons.healthcare,
-            color: COLORS.peach,
-            expenses: [
-                {
-                    id: 11,
-                    title: "Skin Care product",
-                    description: "skin care",
-                    location: "ByProgrammers' Pharmacy",
-                    total: 10.00,
-                    status: pendingStatus,
-                },
-                {
-                    id: 12,
-                    title: "Lotion",
-                    description: "Lotion",
-                    location: "ByProgrammers' Pharmacy",
-                    total: 50.00,
-                    status: confirmStatus,
-                },
-                {
-                    id: 13,
-                    title: "Face Mask",
-                    description: "Face Mask",
-                    location: "ByProgrammers' Pharmacy",
-                    total: 50.00,
-                    status: pendingStatus,
-                },
-                {
-                    id: 14,
-                    title: "Sunscreen cream",
-                    description: "Sunscreen cream",
-                    location: "ByProgrammers' Pharmacy",
-                    total: 50.00,
-                    status: pendingStatus,
-                },
-            ],
+          id: 4,
+          name: "Subventions",
+          icon: icons.grant,
+          cat: income,
+          color: COLORS.red,
+          data: [
+            {
+              id: 1,
+              description: "Subvention pour le projet de recherche",
+              total: 5000.00,
+              date: "2023-04-20"
+            },
+          ],
         },
         {
-            id: 5,
-            name: "Sports",
-            icon: icons.sports_icon,
-            color: COLORS.purple,
-            expenses: [
-                {
-                    id: 15,
-                    title: "Gym Membership",
-                    description: "Monthly Fee",
-                    location: "ByProgrammers' Gym",
-                    total: 45.00,
-                    status: pendingStatus,
-                },
-                {
-                    id: 16,
-                    title: "Gloves",
-                    description: "Gym Equipment",
-                    location: "ByProgrammers' Gym",
-                    total: 15.00,
-                    status: confirmStatus,
-                },
-            ],
+          id: 5,
+          name: "Investissements",
+          icon: icons.investment,
+          cat: income,
+          color: COLORS.purple,
+          data: [
+            {
+              id: 1,
+              description: "Achat d'un immeuble à usage commercial",
+              total: 100000.00,
+              date: "2023-04-30"
+            },
+          ],
         },
-        {
-            id: 6,
-            name: "Clothing",
-            icon: icons.cloth_icon,
-            color: COLORS.red,
-            expenses: [
-                {
-                    id: 17,
-                    title: "T-Shirt",
-                    description: "Plain Color T-Shirt",
-                    location: "ByProgrammers' Mall",
-                    total: 20.00,
-                    status: pendingStatus,
-                },
-                {
-                    id: 18,
-                    title: "Jeans",
-                    description: "Blue Jeans",
-                    location: "ByProgrammers' Mall",
-                    total: 50.00,
-                    status: confirmStatus,
-                },
-            ],
-        }
-    ]
+      ];
 
     const categoryListHeightAnimationValue = useRef(new Animated.Value(115)).current;
 
