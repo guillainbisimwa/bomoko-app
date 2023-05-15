cbe59852cbbef7952eb3a8dc0338514a6303677e
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-05-15 17:10:25 +0200

    Add INtro screens

---
+++ screens/IntroScreen/IntroSlides.js
@@ -0,0 +1,26 @@
+//slide
+const slides = [
+  {
+    id: 1,
+    lable: 'Bienvenue',
+    subtitle: ' Simplifiez votre vie avec BOMOKO',
+    des: 'Notre application de portefeuille électronique facilite vos calculs de revenus et vos dépenses au quotidien. Oubliez les tracas liés au calcul de rentabilite.',
+    imageUrl: require('./../../assets/img/logo1.png'),
+  },
+  {
+    id: 2,
+    lable: 'Interface conviviale',
+    subtitle: 'Accès facile aux fonctionnalités',
+    des: "Profitez de notre interface utilisateur intuitive conçue pour faciliter la gestion de votre portefeuille électronique. Lancez-vous et profitez d'un parcours financier sans soucis avec BOMOKO.",
+    imageUrl: require('./../../assets/img/12.png'),
+  },
+
+  {
+    id: 3,
+    lable: 'Vos revenus et dépenses',
+    subtitle: "Suivez vos finances en un coup d'œil",
+    des: 'Gardez une trace claire de vos revenus et dépenses. Catégorisez vos dépenses et obtenez des informations détaillées sur votre situation financière. Atteignez vos objectifs financiers avec BOMOKO.',
+    imageUrl: require('./../../assets/img/11.png'),
+  },
+];
+export default slides;
---
+++ screens/IntroScreen/components/Slide.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Dimensions, StyleSheet, Image } from 'react-native';
+
+const { height, width } = Dimensions.get('window');
+export const Slide = ({ imageUrl }) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={{
+          resizeMode: 'contain',
+          width: '90%',
+          height: height / 2,
+        }}
+        source={imageUrl}
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 45,
+    flex: 1,
+    width,
+    alignItems: 'center',
+  },
+});
