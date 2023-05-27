fd053533c70a8548cc7f8eb1fd5bc345c6eba159
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-05-27 21:37:13 +0200

    Add Shared item component

---
+++ screens/ProductScreen/components/ShareItem.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { TouchableOpacity, Share } from 'react-native';
+//icon
+import { FontAwesome } from '@expo/vector-icons';
+
+const ShareItem = ({ imageURL, title, message, color }) => {
+  const onShare = async () => {
+    try {
+      const result = await Share.share({
+        title: title,
+        message: message,
+        url: imageURL,
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+  return (
+    <TouchableOpacity onPress={onShare}>
+      <FontAwesome name="share-square-o" size={25} color="white" />
+    </TouchableOpacity>
+  );
+};
+
+export default ShareItem;
