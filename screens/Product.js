5c3b87c73134d86bfc3fc2066a71d7e277e811b0
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-06-05 00:11:50 +0200

    Add Product component

--- screens/Product.js
+++ screens/Product.js
@@ -1,12 +1,54 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  interpolate,
+  Extrapolation,
+  Extrapolate,
+} from 'react-native-reanimated';
 
-const Product = ({ navigation }) => {
+export const HEADER_IMAGE_HEIGHT = Dimensions.get('window').width / 3;
+
+export default function Product() {
+  const scrollY = useSharedValue(0);
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (e) => {
+      scrollY.value = e.contentOffset.y;
+    },
+  });
+  const animatedStyles = useAnimatedStyle(() => {
+    const scale = interpolate(scrollY.value, [-100, 0], [2, 1], {
+      extrapolate: Extrapolate.CLAMP,
+    });
+
+    return {
+      transform: [{ scale: scale }],
+    };
+  });
+
   return (
-    <View>
-      <Text> Product</Text>
+    <View style={{ flex: 1, alignItems: 'center' }}>
+      <Animated.View
+        style={[
+          {
+            position: 'absolute',
+            top: 20,
+            left: 0,
+            width: 20,
+            height: 20,
+            backgroundColor: 'blue',
+          },
+          animatedStyles,
+        ]}
+      />
+
+      {/* <Animated.ScrollView
+        scrollEventThrottle={1}
+        style={StyleSheet.absoluteFill}
+        onScroll={scrollHandler}
+      ></Animated.ScrollView> */}
     </View>
   );
-};
-
-export default Product;
+}
