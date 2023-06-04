be15e9b9e0c6df303b9e8b800d91f42b906d1290
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-06-05 00:11:04 +0200

    Add Prod reducer to Store

--- redux/Store.js
+++ redux/Store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import appReducer from './appReducer';
 import authReducer from './authReducer';
 import catReducer from './catReducer';
+import prodReducer from './prodReducer';
 
 export const store = configureStore({
   reducer: {
     user: authReducer,
     categories: catReducer,
     app: appReducer,
+    products: prodReducer,
   },
 });
 
