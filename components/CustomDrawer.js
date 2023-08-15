84a72d265260ea01e913e7860bf3ddaac11cfb54
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-08-15 18:41:14 +0300

    Update version

--- components/CustomDrawer.js
+++ components/CustomDrawer.js
@@ -42,7 +42,7 @@ const CustomDrawer = (props) => {
                 marginLeft: 5,
               }}
             >
-              Version 4.1.0
+              Version 4.2.0
             </Text>
           </View>
         </TouchableOpacity>
