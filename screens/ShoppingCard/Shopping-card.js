4d09f980f38163b7fc86231486cfa0da7e4a8f02
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-08-20 22:49:34 +0300

    Create a default template of ShoppingCard screen

---
+++ screens/ShoppingCard/Shopping-card.js
@@ -0,0 +1,12 @@
+import { Text } from "react-native-paper"
+import Block from "../Product/Block"
+
+const ShoppingCard = () => {
+    return (
+        <Block>
+            <Text>ShoppingCard</Text>
+        </Block>
+    )
+};
+
+export default ShoppingCard;
