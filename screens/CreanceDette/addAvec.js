50ee2b6059e0026bed2a3ddcb9bfccf7fa8c4f3c
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-09-02 01:33:11 +0200

    Add addAvec screen

--- screens/CreanceDette/addAvec.js
+++ screens/CreanceDette/addAvec.js
@@ -1,208 +1,189 @@
-import React, {  useState } from 'react';
-import { Image, ScrollView, StyleSheet, TouchableOpacity, View, useWindowDimensions } from 'react-native';
-import { COLORS, FONTS, SIZES, icons } from '../../constants';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
-import {Text, Provider, Badge, Divider, Chip, MD3Colors, ProgressBar } from 'react-native-paper';
-import { ImageBackground } from 'react-native';
-import Block from '../Product/Block';
-import { SceneMap, TabBar, TabView } from 'react-native-tab-view';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchAvecs } from '../../redux/avecReducer';
+const addAvec = ({ onAddAvec }) => {
+  const [name, setName] = useState('Your AVEC Name');
+  const [detail, setDetail] = useState('Details about your AVEC');
+  const [amount, setAmount] = useState('1000');
+  const [currency, setCurrency] = useState('USD');
+  const [cycleName, setCycleName] = useState('moi');
+  const [cycleNumber, setCycleNumber] = useState('9');
+  const [nbrPartMax, setNbrPartMax] = useState('5');
+  const [nbrPartMin, setNbrPartMin] = useState('1');
+  const [interest, setInterest] = useState('5');
+  const [fraisAdhesion, setFraisAdhesion] = useState('10');
+  const [debutOctroiCredit, setDebutOctroiCredit] = useState('2023-05-10');
+  const [finOctroiCredit, setFinOctroiCredit] = useState('2023-08-10');
+  const [startDate, setStartDate] = useState('2023-05-01');
+  const [endDate, setEndDate] = useState('2023-12-01');
 
+  const handleAddAvec = () => {
+    // Create an AVEC object with the form data
+    const avec = {
+      name,
+      detail,
+      amount: Number(amount),
+      currency,
+      cycle: {
+        name: cycleName,
+        number: Number(cycleNumber),
+      },
+      nbrPart: {
+        max: Number(nbrPartMax),
+        min: Number(nbrPartMin),
+      },
+      interest,
+      frais_Adhesion: Number(fraisAdhesion),
+      debut_octroi_credit: debutOctroiCredit,
+      fin_octroi_credit: finOctroiCredit,
+      startDate,
+      endDate,
+    };
 
-const addAvec = ({ navigation, route }) => {
-  const [ badgePanding, setBadgePanding ] = useState(0);
-  const layout = useWindowDimensions();
-  const [index, setIndex] = useState(0);
-
-  const [routes, SetRoutes] = useState([
-    { key: "first", title: `Route1`},
-    { key: "second", title: `Route2` },
-    { key: "third", title: "Route3" },
-  ]);
-
-  const dispatch = useDispatch();
-  const avecs = useSelector((state) => state.avecs.avecs); // Replace 'avecs' with your slice name
-
-  useEffect(() => {
-    // Dispatch the fetchAvecs async thunk when the component mounts
-    dispatch(fetchAvecs());
-  }, [dispatch]); // Make sure to include dispatch in the dependency array
-
-
-  const renderTabBar = (props) => (
-    <TabBar
-      {...props}
-      indicatorStyle={{
-        backgroundColor: COLORS.peach,
-        padding:3,
-      }}
-      style={{
-        backgroundColor: COLORS.white,
-        padding: 10,
-        borderRadius:10,
-        marginTop: -5
-      }}
-      renderLabel={({ focused, route }) => (
-        <Text  style={[{ color: focused ? COLORS.black : COLORS.gray }]}>
-          {route.title}
-        </Text>
-      )}
-    />
-  );
-
- 
-  function renderNavBar() {
-    return (
-      <View
-        style={{
-          flexDirection: 'row',
-          paddingTop: SIZES.base * 3,
-          justifyContent: 'space-between',
-          paddingHorizontal: SIZES.padding,
-        }}
-      >
-        <View
-          style={{
-            flexDirection: 'row',
-          }}
-        >
-          <TouchableOpacity
-            style={{
-              paddingRight: SIZES.base * 2,
-            }}
-            onPress={() => {
-              console.log('Menu');
-              navigation.openDrawer();
-            }}
-          >
-            <Image
-              source={icons.menu}
-              style={{
-                width: SIZES.base * 4,
-                height: SIZES.base * 3,
-                tintColor: COLORS.white,
-              }}
-            />
-          </TouchableOpacity>
-          
-          <View>
-            <Text style={{ color: COLORS.white, ...FONTS.h2 }}>BOMOKO Cash</Text>
-            <Text style={{ ...FONTS.h3, color: COLORS.gray }}>(Créance et Dettes)</Text>
-        </View>
-        </View>
-        
-        <View style={{ flexDirection: 'row' }}>
-        <TouchableOpacity
-          style={{ justifyContent: 'center', alignItems: 'flex-end', width: 50 }}
-          onPress={() => {
-            console.log('shopping');
-            //console.log("Token --",JSON.parse(token).user.user.username);
-            
-          }}
-        >
-          <Image
-            source={icons.shopping}
-            style={{
-              width: 30,
-              height: 30,
-              tintColor: COLORS.white,
-            }}
-          />
-            <Badge style={{ position:"absolute", top:2, right:-8 }}>{badgePanding}</Badge>
-
-        </TouchableOpacity>
-        
-      <View
-        style={{
-          justifyContent: 'center', alignItems: 'flex-end', width: 50
-        }}>
-        
-      </View>
-         
-      </View>
-      </View>
-    );
-  }
+    // Pass the new AVEC object to a parent component or Redux action
+    onAddAvec(avec);
+  };
 
   return (
-   
-    <Provider>
-    <ImageBackground
-      style={{ flex: 1, position: 'absolute', height: '100%', width: '100%' }}
-      source={require('./../../assets/login1_bg.png')}
-      blurRadius={10}
-    ></ImageBackground>
-    <View style={{ flex: 1 }}>
-      {/* Nav bar section */}
-      {renderNavBar()}
+    <View style={styles.container}>
+      <Text style={styles.label}>Name:</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+        placeholder="Enter AVEC name"
+      />
 
-      
+      <Text style={styles.label}>Detail:</Text>
+      <TextInput
+        style={styles.input}
+        value={detail}
+        onChangeText={setDetail}
+        placeholder="Enter AVEC detail"
+      />
+
+      <Text style={styles.label}>Amount:</Text>
+      <TextInput
+        style={styles.input}
+        value={amount}
+        onChangeText={setAmount}
+        keyboardType="numeric"
+        placeholder="Enter AVEC amount"
+      />
+
+      <Text style={styles.label}>Currency:</Text>
+      <TextInput
+        style={styles.input}
+        value={currency}
+        onChangeText={setCurrency}
+        placeholder="Enter currency (e.g., USD)"
+      />
+
+      <Text style={styles.label}>Cycle Name:</Text>
+      <TextInput
+        style={styles.input}
+        value={cycleName}
+        onChangeText={setCycleName}
+        placeholder="Enter cycle name"
+      />
+
+      <Text style={styles.label}>Cycle Number:</Text>
+      <TextInput
+        style={styles.input}
+        value={cycleNumber}
+        onChangeText={setCycleNumber}
+        keyboardType="numeric"
+        placeholder="Enter cycle number"
+      />
+
+      <Text style={styles.label}>Max Participants:</Text>
+      <TextInput
+        style={styles.input}
+        value={nbrPartMax}
+        onChangeText={setNbrPartMax}
+        keyboardType="numeric"
+        placeholder="Enter max participants"
+      />
+
+      <Text style={styles.label}>Min Participants:</Text>
+      <TextInput
+        style={styles.input}
+        value={nbrPartMin}
+        onChangeText={setNbrPartMin}
+        keyboardType="numeric"
+        placeholder="Enter min participants"
+      />
+
+      <Text style={styles.label}>Interest (%):</Text>
+      <TextInput
+        style={styles.input}
+        value={interest}
+        onChangeText={setInterest}
+        keyboardType="numeric"
+        placeholder="Enter interest rate"
+      />
+
+      <Text style={styles.label}>Frais d'Adhesion:</Text>
+      <TextInput
+        style={styles.input}
+        value={fraisAdhesion}
+        onChangeText={setFraisAdhesion}
+        keyboardType="numeric"
+        placeholder="Enter frais d'adhesion"
+      />
+
+      <Text style={styles.label}>Début Octroi Credit:</Text>
+      <TextInput
+        style={styles.input}
+        value={debutOctroiCredit}
+        onChangeText={setDebutOctroiCredit}
+        placeholder="Enter début octroi credit"
+      />
+
+      <Text style={styles.label}>Fin Octroi Credit:</Text>
+      <TextInput
+        style={styles.input}
+        value={finOctroiCredit}
+        onChangeText={setFinOctroiCredit}
+        placeholder="Enter fin octroi credit"
+      />
+
+      <Text style={styles.label}>Start Date:</Text>
+      <TextInput
+        style={styles.input}
+        value={startDate}
+        onChangeText={setStartDate}
+        placeholder="Enter start date"
+      />
+
+      <Text style={styles.label}>End Date:</Text>
+      <TextInput
+        style={styles.input}
+        value={endDate}
+        onChangeText={setEndDate}
+        placeholder="Enter end date"
+      />
+
+      <Button title="Add AVEC" onPress={handleAddAvec} />
     </View>
-    </Provider>
   );
 };
 
 const styles = StyleSheet.create({
-
-  topMenu: {
-    margin: 20,
+  container: {
+    padding: 20,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
     padding: 10,
-    //elevation: 5,
-    flex: 1
+    marginBottom: 15,
   },
-  text: {
-    marginBottom:10
-  },
-  myTopCard:{
-    backgroundColor: COLORS.white,
-    padding: 10,
-    borderTopEndRadius: 10,
-    borderTopStartRadius: 10
-  },
-  imgs: {
-    flexDirection: 'row',
-    marginVertical:10,
-  },
-  div:{
-    marginVertical:10
-  },
-  img: {
-    borderRadius: SIZES.base * 3,
-    backgroundColor:COLORS.red,
-    borderWidth:2,
-    borderColor: COLORS.purple,
-    width: SIZES.base * 5,
-    height: SIZES.base * 5,
-    tintColor: COLORS.black,
-  },
-  card: {
-    backgroundColor: COLORS.lightGray,
-    padding:10,
-    borderRadius:10,
-    elevation:5
-  },
-  moreImagesText: {
-    flex:1,
-    alignSelf:'center', 
-    marginLeft:10
-  },
-  bold:{
-    fontWeight:'bold'
-  },
-  boldGrey:{
-    fontWeight:'bold',
-    color:COLORS.gray,
-    textTransform: 'uppercase'
-  },
-  small:{
-    fontSize:13,
-    color:COLORS.peach
-  },
-  normal:{
-    color:COLORS.gray,
-    marginTop: 10
-  }
 });
 
 export default addAvec;
