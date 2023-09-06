a4ac168ee54a4f57e8cac891f7c2f254dc558c62
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-09-06 16:18:31 +0200

    Fetch user id from Local Storage

--- screens/Product/productScreen.js
+++ screens/Product/productScreen.js
@@ -7,17 +7,26 @@ import Product_service from './Product_service';
 import LottieView from 'lottie-react-native';
 
 import { ImageBackground, Alert } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Badge, Divider, Menu, Provider } from 'react-native-paper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { loginSuccess } from '../../redux/authReducer';
 
 const ProductScreen = ({ navigation, route }) => {
 
-  const { user } = useSelector((state) => state.user);
+  const { user } = useSelector((state) => state);
   const [visibleMenu, setVisibleMenu] = useState(false);
 
   const [token, setToken] = useState(null);
+  const [connectedUser, setConnectedUser] = useState(null);
 
   const [ badgePanding, setBadgePanding ] = useState(0);
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    checkLoginStatus();
+  },[connectedUser])
 
   // Menu
   const openMenu = () => setVisibleMenu(true);
@@ -27,9 +36,32 @@ const ProductScreen = ({ navigation, route }) => {
   const handleLogin = () => {
     // Dispatch the logoutUser action
     closeMenu();
-    //dispatch(logoutUser());
-
     navigation.navigate('AuthScreen');
+  };
+
+  const checkLoginStatus = async () => {
+    try {
+      const value = await AsyncStorage.getItem('user');
+      // AsyncStorage.clear();
+
+      console.log('AsyncStorage-user', value);
+      console.log("");
+      console.log('State-user', connectedUser);
+      
+      if (value !== null) {
+        // dispatch(loginSuccess(value));
+        // AsyncStorage.clear();
+        // dispatch(logoutUser());
+        setConnectedUser(value);
+        
+      } else {
+        //setLoading(false);
+        //dispatch(setUnInstalled());
+      }
+    } catch (error) {
+      console.log('Error retrieving installation status:', error);
+      //setLoading(false);
+    }
   };
 
 
@@ -176,118 +208,7 @@ const ProductScreen = ({ navigation, route }) => {
 };
 
 const styles = StyleSheet.create({
-  img: {
-    width: 50,
-    height: 50,
-    borderRadius: 50,
-    borderWidth: 4,
-    borderColor: COLORS.white,
-  },
-  marginRight: {
-    marginRight: 5,
-  },
-  input: {
-    width: '100%',
-    height: 55,
-    backgroundColor: COLORS.white,
-    borderRadius: 16,
-    paddingHorizontal: 44,
-    fontSize: 20,
-  },
-  toggle: {
-    position: 'absolute',
-    top: 19,
-    left: 15,
-  },
-  container: {
-    borderRadius: 16,
-    marginRight: 15,
-    width: SIZES.width - 100,
-  },
-  imgFood: {
-    width: '100%',
-    height: (SIZES.width - 100) / 2,
-    borderRadius: 16,
-    marginBottom: 10,
-    borderWidth:1,
-    borderColor: COLORS.gray
-  },
-  info: {
-    backgroundColor: COLORS.grey,
-    padding: 7,
-    borderRadius: 10,
-  },
-  cat: {
-    //width: SIZES.width / 4 - 4,
-    width: '33%',
-    height: SIZES.width / 5,
-    marginRight: 2,
-    borderRadius: 10,
-    //backgroundColor: COLORS.purple,
-    elevation: 4,
-    padding: 5,
-  },
-  recommended: {
-    width: SIZES.width / 2.5,
-    marginRight: 15,
-    borderRadius: 10,
-    elevation: 2,
-    padding: 10,
-  },
-  imgRecommended: {
-    width: '100%',
-    height: SIZES.width / 4,
-    borderRadius: 10,
-    marginBottom: 5,
-  },
-  listContainer: {
-    borderRadius: 20,
-  },
-  tab: {
-    marginRight: 20,
-    paddingBottom: 5,
-  },
-  active: {
-    borderBottomColor: COLORS.peach,
-    borderBottomWidth: 5,
-    width: 30,
-    paddingBottom: 5,
-  },
-  current: {
-    color: COLORS.grey,
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  currentActive: {
-    color: COLORS.primary,
-  },
-  price: {
-    position: 'absolute',
-    zIndex: 100,
-    backgroundColor: COLORS.peach,
-    padding: 10,
-    borderRadius: 20,
-    elevation: 5,
-  },
-  like: {
-    position: 'absolute',
-    zIndex: 100,
-    backgroundColor: COLORS.white,
-    borderRadius: 25,
-    right: 0,
-    margin: SIZES.base * 2,
-  },
-  fab: {
-    position: 'absolute',
-    margin: 16,
-    //backgroundColor: '#ff0000',
-    right: 0,
-    bottom: 0,
-  },
-  animation: {
-    width: 50,
-    height: 50,
-  },
+  
 });
 
 export default ProductScreen;
