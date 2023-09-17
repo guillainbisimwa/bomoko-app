2b0078320808ce8138b482391d4ba0ebf477d1e1
guillainbisimwa <guillainbisimwa@hotmail.fr>
2023-09-18 01:05:48 +0200

    Add transaction Component

---
+++ screens/CreanceDette/Transaction.js
@@ -0,0 +1,125 @@
+import React, { useState } from 'react';
+import { Image, StyleSheet, TouchableOpacity,View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+import { COLORS, SIZES, icons, FONTS } from '../../constants';
+import { Text } from '../../components';
+
+const Transaction = (props,{ navigation } ) => {
+  // console.log(props.user);
+  // console.log();
+  // subtitle='Contibution solidaire' topRight={1} 
+  //             bottomRight='10 sep 2023'
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        console.log(props.user.user.name);
+        // navigation.navigate('Profile', { user: props.user.user})
+        // props.navigation.navigate('Profile', {
+        //   userId: props.user.user._id,
+        //   user: props.user.user
+        // })
+      }}
+    >
+      <View
+        style={{
+          marginVertical: SIZES.padding / 3.7,
+          marginHorizontal:SIZES.padding/2,
+          borderRadius: SIZES.radius,
+          backgroundColor: COLORS.white,
+            shadowColor: COLORS.black,
+            shadowOffset: { width: 0, height: 2 },
+            shadowOpacity: 0.1,
+            shadowRadius: 13,
+            elevation: 2,
+            
+        }}
+      >
+        {/* Title */}
+        <View
+          style={{
+            flexDirection: 'row',
+            padding: SIZES.padding / 2,
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <View
+            style={{
+              width: '65%',
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}
+          >
+            <View
+              style={{
+                height: 50,
+                width: 50,
+                borderRadius: 25,
+                backgroundColor: COLORS.lightGray,
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginRight: SIZES.base,
+              }}
+            >
+              { props.user.user.profile_pic  ? (
+                <Image
+                  source={{ uri: props.user.user.profile_pic  }}
+                  style={{ width: 40, height: 40, borderRadius:20, borderWidth:1,
+                  borderColor: COLORS.white}}
+                />
+              ) : (
+                <Image
+                  source={icons.investment}
+                  style={{
+                    width: 30,
+                    height: 30,
+                    tintColor: COLORS.black,
+                  }}
+                />
+              )}
+
+            </View>
+            <View>
+              <Text numberOfLines={1} style={{ ...FONTS.h3, color: COLORS.black }}>{
+                props.user.user.admin? props.user.user?.name: props.user.user?.name 
+              }
+              </Text>
+              <Text
+                numberOfLines={1}
+                style={{
+                  overflow: 'hidden',
+                  ...FONTS.body5,
+                  flexWrap: 'wrap',
+                  color: COLORS.darkgray,
+                }}
+              >
+              {props.subtitle}
+              </Text>
+            </View>
+            
+          </View>
+          
+          <View style={{ alignItems: 'flex-end' }}>
+          <>
+            <Text style={{ ...FONTS.h5, color: COLORS.red }}>{props.topRight} {props.currency}</Text>
+              <View style={{ flexDirection: 'row' }}>
+          
+                <Text style={{ marginBottom: SIZES.base, color: COLORS.darkgray, ...FONTS.body5 }}>
+                 {props.bottomRight}
+                </Text>
+            </View>
+          </>
+        </View>
+    
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default Transaction;
